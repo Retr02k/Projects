0241e5038d9d127fe7844306ced70afbd659ea1b
Retr02k <pedro210332@gmail.com>
2024-10-27 16:38:38 +0000

    ft_isascii

---
+++ libft.a/ft_isasciic.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "libft.h"
+
+int	ft_isacii(int	c)
+{
+	return (c >= 0 && c <= 127);
+}
+/* 
+int main (void)
+{
+	printf("%d\n", ft_isacii(65));		// Output: 1 (ASCII 'A)
+	printf("%d\n", ft_isacii(1128));	// Output: 0 (Not ASCII)
+	printf("%d\n", ft_isacii(-1));		// Output: 0 (Not ASCII)
+
+	return (0);
+} */
