737f4878a5b1d21bf45e275f5c9be82c03fcac3d
Retr02k <pedro210332@gmail.com>
2025-01-23 17:05:22 +0000

    added ft_lstclear.c to libft.h && created ft.lstclear.c

---
+++ libft.a/ft_lstclear.c
@@ -0,0 +1,9 @@
+#include	"libft.h"
+#include	<stdio.h>
+#include	<stdlib.h>
+
+void ft_lstclear(t_list **lst, void (*del)(void*))
+{
+	
+
+}
--- libft.a/libft.h
+++ libft.a/libft.h
@@ -51,6 +51,7 @@ t_list;
 // Function prototypes for linked list operations
 void	ft_lstadd_back(t_list **lst, t_list *new);
 void 	ft_lstadd_front(t_list **lst, t_list *new);
+void	ft_lstclear(t_list **lst, void (*del)(void*));
 void	ft_lstdelone(t_list *lst, void (*del)(void*));
 t_list 	*ft_lstlast(t_list *lst);
 t_list  *ft_lstnew(void *content);
